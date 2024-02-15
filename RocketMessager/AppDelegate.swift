6dd588e0c90ad665cfd7eff27c7fb31fc3024fb2
Anton Makarenkov <anton.makarenkov@interactive-ventures.com>
2024-02-15 18:03:38 +0400

    [code] Initial commit

---
+++ RocketMessager/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RocketMessager
+//
+//  Created by Anton Makarenkov on 15.02.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let screenBounds = UIScreen.main.bounds
+        window = UIWindow(frame: screenBounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ RocketMessager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RocketMessager
+//
+//  Created by Anton Makarenkov on 15.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+}
+
