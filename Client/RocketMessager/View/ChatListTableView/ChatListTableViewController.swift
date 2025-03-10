306010c1c503e7bb3146db048d41edc7d8023122
Anton Makarenkov <anton.makarenkov@interactive-ventures.com>
2025-03-11 01:57:24 +0400

    feat: added ChatListTableView with mock cell

--- Client/RocketMessager/View/ChatListTableView/ChatListTableViewController.swift
+++ Client/RocketMessager/View/ChatListTableView/ChatListTableViewController.swift
@@ -9,19 +9,66 @@ import UIKit
 
 final class ChatListTableViewCell: UITableViewCell {
     static let cellIdentifier = "ChatListTableViewCell"
+        
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        // FIXME: -
+    }
 }
 
-final class ChatListTableViewController: UITableViewController {
+final class ChatListTableViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: ChatListTableViewCell.cellIdentifier,
+            for: indexPath
+        ) as? ChatListTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         
-        super.viewDidLoad()
-        
+        setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    private func setupUI() {
         tableView.register(
             ChatListTableViewCell.self,
             forCellReuseIdentifier: ChatListTableViewCell.cellIdentifier
         )
+        tableView.rowHeight = 64
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ]
+        )
     }
-    
-    
 }
