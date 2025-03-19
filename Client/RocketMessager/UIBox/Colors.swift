58060dce3cac3ac4b8b349b0e26201c6c6e7e7ae
Anton Makarenkov <anton.makarenkov@interactive-ventures.com>
2025-03-19 19:45:18 +0400

    feat: UserList cell layout, DesignSystme refactoring

--- Client/RocketMessager/UIBox/Colors.swift
+++ Client/RocketMessager/UIBox/Colors.swift
@@ -7,22 +7,56 @@
 
 import UIKit
 
+public extension UIColor {
+    
+    // Ala design system
+    enum RM {
+        static var label: UIColor {
+            UIColor.white
+        }
+        static var secondaryLabel: UIColor {
+            UIColor(hexString: "000E08")
+        }
+        
+        static var background: UIColor {
+            UIColor(hexString: "000E08")
+        }
+        
+        static var backgroundSecondary: UIColor {
+            UIColor(hexString: "000E08")
+        }
+        
+        static var gray: UIColor {
+            UIColor(hexString: "797C7B")
+        }
+        
+        static var gray2: UIColor {
+            UIColor(hexString: "797C7B")
+        }
+    }
+}
+
 enum DesignColors {
     enum Chat {
         static let userCellBackground = UIColor(hexString: "20A090")
         static let otherCellBackground = UIColor(hexString: "F2F7FB")
-        static let userCellTextColor = UIColor.white
-        static let otherCellTextColor = UIColor(hexString: "000E08")
-        static let timeLabelCellTextColor = UIColor(hexString: "797C7B") 
+        static let userCellTextColor = UIColor.RM.label
+        static let otherCellTextColor = UIColor.RM.secondaryLabel
+        static let timeLabelCellTextColor = UIColor.RM.gray
     }
     
     enum ChatTitle {
-        static let tileTextColor = UIColor(hexString: "000000")
-        static let subtitleTextColor = UIColor(hexString: "797C7B")
+        static let titleTextColor = UIColor(hexString: "000000")
+        static let subtitleTextColor = UIColor.RM.gray
     }
     
     enum ChatInput {
         static let inputTextFieldBackgroundColor = UIColor(hexString: "F3F6F6")
+    }
+    
+    enum UserList {
+        static let titleTextColor = UIColor(hexString: "000000")
+        static let statusTextColor = UIColor.RM.gray2
     }
 }
 
--- Client/RocketMessager/View/ChatListTableView/ChatListTableViewCell.swift
+++ Client/RocketMessager/View/ChatListTableView/ChatListTableViewCell.swift
@@ -9,6 +9,24 @@ import UIKit
 
 final class ChatListTableViewCell: UITableViewCell {
     
+    enum Config {
+        enum Layout {
+            static let horizontalInset = 24.0
+            static let verticalInset = 15.0
+            static let imageSizeSide: CGFloat = 50.0
+        }
+        
+        enum Colors {
+            static let nameLabelTextColor: UIColor = DesignColors.UserList.titleTextColor
+            static let statusLabelTextColor: UIColor = DesignColors.UserList.statusTextColor
+        }
+        
+        enum Fonts {
+            static let nameLabelFont = UIFont.systemFont(ofSize: 20, weight: .medium)
+            static let statusLabelFont = UIFont.systemFont(ofSize: 12, weight: .medium)
+        }
+    }
+    
     static let cellIdentifier = "ChatListTableViewCell"
     
     // MARK: - Properties
@@ -24,17 +42,22 @@ final class ChatListTableViewCell: UITableViewCell {
     
     private var nameLabel: UILabel = {
         let label = UILabel()
+        label.font = Config.Fonts.nameLabelFont
+        label.textColor = Config.Colors.nameLabelTextColor
         return label
     }()
     
     private var statusLabel: UILabel = {
         let label = UILabel()
+        label.font = Config.Fonts.statusLabelFont
+        label.textColor = Config.Colors.statusLabelTextColor
         return label
     }()
     
     private lazy var stackView: UIStackView = {
         var stackView = UIStackView(arrangedSubviews: [nameLabel, statusLabel])
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 6
         stackView.axis = .vertical
         return stackView
     }()
@@ -85,7 +108,10 @@ final class ChatListTableViewCell: UITableViewCell {
         NSLayoutConstraint.activate([
             stackView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
             stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
+            stackView.bottomAnchor.constraint(
+                lessThanOrEqualTo: contentView.bottomAnchor,
+                constant: -15
+            )
         ])
     }
 }
